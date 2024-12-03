c86f7aecc4ed73529f830ef5099f54a686a6f221
Abdullah <abdullahsalman772@gmail.com>
2024-12-03 20:41:45 +0500

    deployment

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,9 @@ require("dotenv").config();
 require("./config/db");
 const PORT = process.env.PORT || 5000;
 
-// app.get("/ping", (req, res) => {
-//   res.send("pong");
-// });
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -20,21 +20,21 @@ app.use("/auth", AuthRouter);
 app.use("/expenses", ensureAuthenticated, ExpenseRouter);
 app.use("/products", ProductRouter);
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use("/uploads", express.static("/var/data/uploads"));
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   const __dirname = path.resolve();
+//   app.use("/uploads", express.static("/var/data/uploads"));
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  const __dirname = path.resolve();
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   const __dirname = path.resolve();
+//   app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+//   app.get("/", (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log("Server is running on Port", PORT);
